538b4650d47f083489d71e853e759616281fdc38
River Champeimont <4948143+rchampeimont@users.noreply.github.com>
2026-07-29 20:40:31 -0400

    Examples of weights

--- stocks/my_stocks.ino
+++ stocks/my_stocks.ino
@@ -4,9 +4,9 @@
 struct stock compIndex = { "IVV", 695.32 }; // S&P 500 ETF
 
 struct stock myStocks[] = {
-  { "RMS.PAR", 2150.0 }, // – Hermes (France)
-  { "COST", 914.252 }, // – Costco (US)
-  { "ALB", 176.80 } // - Albemarle Corp (US)
+  { 1.0, "RMS.PAR", 2150.0 }, // – Hermes (France)
+  { 1.5, "COST", 914.252 }, // – Costco (US)
+  { 0.7, "ALB", 176.80 } // - Albemarle Corp (US)
 };
 
 size_t stockCount = sizeof(myStocks) / sizeof(myStocks[0]);
